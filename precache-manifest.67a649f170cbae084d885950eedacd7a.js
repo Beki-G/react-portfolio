bcf5bddb253a297f26476c1f0e7627fd2de56b9d
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-10-23 20:50:39 -0700

    Updates

---
+++ precache-manifest.67a649f170cbae084d885950eedacd7a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba59b2b8d3b4b474494b74a4d2fa59e7",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "865eaf31cadbf5cfdb08",
+    "url": "/react-portfolio/static/css/main.d1b0e217.chunk.css"
+  },
+  {
+    "revision": "35a4821adbdfb1c381dc",
+    "url": "/react-portfolio/static/js/2.de6f349f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-portfolio/static/js/2.de6f349f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865eaf31cadbf5cfdb08",
+    "url": "/react-portfolio/static/js/main.279e341a.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "2be97b9fe5c945453d19cfd96a084611",
+    "url": "/react-portfolio/static/media/ShikiAbilitySearch.2be97b9f.gif"
+  },
+  {
+    "revision": "e2a259988158319a8f438f7530964bf7",
+    "url": "/react-portfolio/static/media/book.e2a25998.gif"
+  },
+  {
+    "revision": "93c138f4047fc06405070dc03dcc21b0",
+    "url": "/react-portfolio/static/media/codelocker.93c138f4.png"
+  },
+  {
+    "revision": "c0ce54bd40694ea22caaa23c9ad42cb0",
+    "url": "/react-portfolio/static/media/coding.c0ce54bd.jpg"
+  },
+  {
+    "revision": "d6fd84736f5a6c958450512da12a78e0",
+    "url": "/react-portfolio/static/media/emoployee.d6fd8473.gif"
+  },
+  {
+    "revision": "cb56c6843e0908a4b21f742e1a4ad0cd",
+    "url": "/react-portfolio/static/media/initials.cb56c684.png"
+  },
+  {
+    "revision": "e5c03c5123e11b7b0003e21da75a2150",
+    "url": "/react-portfolio/static/media/jsQuiz.e5c03c51.png"
+  },
+  {
+    "revision": "ba0aa1391abb0fdbeea999b704c557fa",
+    "url": "/react-portfolio/static/media/login.ba0aa139.png"
+  },
+  {
+    "revision": "df35fdaf99e0ad57add8d05821f0759d",
+    "url": "/react-portfolio/static/media/quote.df35fdaf.png"
+  },
+  {
+    "revision": "26d989f805761e830e200e496ebe42d4",
+    "url": "/react-portfolio/static/media/resume.26d989f8.pdf"
+  },
+  {
+    "revision": "0f4ecd1173845d9b80458fcad7209a8d",
+    "url": "/react-portfolio/static/media/stew.0f4ecd11.jpg"
+  },
+  {
+    "revision": "0e7230b7edf89b7f0dd8b1323fa6cd97",
+    "url": "/react-portfolio/static/media/weather.0e7230b7.png"
+  }
+]);
